b5b9755a36df4bbfeaefb2a001f33c20ee4c4c0a
emarques123 <emarques@frba.utn.edu.ar>
2025-06-01 11:45:54 -0300

    add esquema

--- Entrega 2/Migracion.sql
+++ Entrega 2/Migracion.sql
@@ -7,7 +7,7 @@ GO
 PRINT 'Insertando datos en tabla Provincia...';
 -- ========================================
 
-INSERT INTO Provincia(nombre_prov)
+INSERT INTO LOS_HELECHOS.Provincia(nombre_prov)
 SELECT *
 FROM (
     SELECT Sucursal_Provincia AS p FROM gd_esquema.Maestra
@@ -24,7 +24,7 @@ WHERE p is not null;
 PRINT 'Insertando datos en tabla Localidad...';
 -- ========================================}
 
-INSERT INTO Localidad (nombre_localidad, provincia)
+INSERT INTO LOS_HELECHOS.Localidad (nombre_localidad, provincia)
 SELECT DISTINCT loc, p.cod_prov
 FROM (
     SELECT Sucursal_Localidad AS loc, Sucursal_Provincia AS prov
@@ -36,7 +36,7 @@ FROM (
     SELECT Proveedor_Localidad, Proveedor_Provincia
     FROM gd_esquema.Maestra
 ) AS all_loc
-JOIN Provincia p ON all_loc.prov = p.nombre_prov
+JOIN LOS_HELECHOS.Provincia p ON all_loc.prov = p.nombre_prov
 WHERE loc IS NOT NULL;
 
 -- ========================================
@@ -44,7 +44,7 @@ WHERE loc IS NOT NULL;
 PRINT 'Insertando datos en tabla Sucursal...';
 
 -- ========================================
-INSERT INTO Sucursal (localidad, nro_sucursal, direccion, telefono, mail)
+INSERT INTO LOS_HELECHOS.Sucursal (localidad, nro_sucursal, direccion, telefono, mail)
 SELECT DISTINCT 
     l.cod_localidad,
     Sucursal_NroSucursal,
@@ -52,12 +52,12 @@ SELECT DISTINCT
     Sucursal_Telefono,
     Sucursal_Mail
 FROM gd_esquema.Maestra tm
-JOIN Provincia p ON tm.sucursal_provincia = p.nombre_prov
-JOIN Localidad l ON tm.sucursal_localidad = l.nombre_localidad AND l.provincia = p.cod_prov
+JOIN LOS_HELECHOS.Provincia p ON tm.sucursal_provincia = p.nombre_prov
+JOIN LOS_HELECHOS.Localidad l ON tm.sucursal_localidad = l.nombre_localidad AND l.provincia = p.cod_prov
 
 -- ========================================
 -- Inserts para tabla Cliente
--- Detalles: Los clientes estan duplicados por eso utilización una partición y row_number
+-- Detalles: Los clientes estan duplicados por eso utilizaciï¿½n una particiï¿½n y row_number
 PRINT 'Insertando datos en tabla Cliente...';
 -- ========================================
 WITH ClientesUnicos AS (
@@ -69,7 +69,7 @@ WITH ClientesUnicos AS (
     FROM gd_esquema.Maestra
     WHERE Cliente_Dni IS NOT NULL
 )
-INSERT INTO Cliente (dni, localidad, nombre, apellido, fecha_nacimiento, mail, direccion, telefono)
+INSERT INTO LOS_HELECHOS.Cliente (dni, localidad, nombre, apellido, fecha_nacimiento, mail, direccion, telefono)
 SELECT
     Cliente_Dni,
     l.cod_localidad,
@@ -80,8 +80,8 @@ SELECT
     Cliente_Direccion,
     Cliente_Telefono
 FROM ClientesUnicos tm
-JOIN Provincia p ON tm.Cliente_Provincia = p.nombre_prov
-JOIN Localidad l ON tm.Cliente_Localidad = l.nombre_localidad AND l.provincia = p.cod_prov
+JOIN LOS_HELECHOS.Provincia p ON tm.Cliente_Provincia = p.nombre_prov
+JOIN LOS_HELECHOS.Localidad l ON tm.Cliente_Localidad = l.nombre_localidad AND l.provincia = p.cod_prov
 WHERE rn = 1;
 
 -- ========================================
@@ -89,7 +89,7 @@ WHERE rn = 1;
 PRINT 'Insertando datos en tabla Proveedor...';
 -- ========================================
 
-INSERT INTO Proveedor (cuit, razon_social, localidad, direccion, telefono, mail)
+INSERT INTO LOS_HELECHOS.Proveedor (cuit, razon_social, localidad, direccion, telefono, mail)
 SELECT DISTINCT
     Proveedor_Cuit,
     Proveedor_RazonSocial,
@@ -98,14 +98,14 @@ SELECT DISTINCT
     Proveedor_Telefono,
     Proveedor_Mail
 FROM gd_esquema.Maestra tm
-JOIN Provincia p ON tm.Proveedor_Provincia = p.nombre_prov
-JOIN Localidad l ON tm.Proveedor_Localidad = l.nombre_localidad AND l.provincia = p.cod_prov;
+JOIN LOS_HELECHOS.Provincia p ON tm.Proveedor_Provincia = p.nombre_prov
+JOIN LOS_HELECHOS.Localidad l ON tm.Proveedor_Localidad = l.nombre_localidad AND l.provincia = p.cod_prov;
 
 -- ========================================
 -- Inserts para tabla Material
 PRINT 'Insertando datos en tabla Material...';
 -- ========================================
-INSERT INTO Material (nombre, precio)
+INSERT INTO LOS_HELECHOS.Material (nombre, precio)
 SELECT DISTINCT 
     Material_Nombre,
     Material_Precio
@@ -116,20 +116,20 @@ WHERE Material_nombre is not null;
 -- Inserts para tabla Tela (solo tipo 'Tela')
 PRINT 'Insertando datos en tabla Tela...';
 -- ========================================
-INSERT INTO Tela (id_material, color, textura)
+INSERT INTO LOS_HELECHOS.Tela (id_material, color, textura)
 SELECT DISTINCT 
     m.id_material,
     tm.Tela_Color,
     tm.Tela_Textura
 FROM gd_esquema.Maestra tm
-JOIN Material m ON tm.Material_Nombre = m.nombre
+JOIN LOS_HELECHOS.Material m ON tm.Material_Nombre = m.nombre
 WHERE tm.Material_Tipo = 'Tela' AND tm.Tela_Color IS NOT NULL;
 
 -- ========================================
 -- Inserts para tabla Madera (solo tipo 'Madera')
 PRINT 'Insertando datos en tabla Madera...';
 -- ========================================
-INSERT INTO Madera (id_material, nombre, descripcion, color, dureza)
+INSERT INTO LOS_HELECHOS.Madera (id_material, nombre, descripcion, color, dureza)
 SELECT DISTINCT 
     m.id_material,
     tm.Material_Nombre,
@@ -137,21 +137,21 @@ SELECT DISTINCT
     tm.Madera_Color,
     tm.Madera_Dureza
 FROM gd_esquema.Maestra tm
-JOIN Material m ON tm.Material_Nombre = m.nombre
+JOIN LOS_HELECHOS.Material m ON tm.Material_Nombre = m.nombre
 WHERE tm.Material_Tipo = 'Madera' AND tm.Madera_Color IS NOT NULL;
 
 -- ========================================
 -- Inserts para tabla Relleno (solo tipo 'Relleno')
 PRINT 'Insertando datos en tabla Relleno...';
 -- ========================================
-INSERT INTO Relleno (id_material, nombre, descripcion, densidad)
+INSERT INTO LOS_HELECHOS.Relleno (id_material, nombre, descripcion, densidad)
 SELECT DISTINCT 
     m.id_material,
     tm.Material_Nombre,
     tm.Material_Descripcion,
     tm.Relleno_Densidad
 FROM gd_esquema.Maestra tm
-JOIN Material m ON tm.Material_Nombre = m.nombre
+JOIN LOS_HELECHOS.Material m ON tm.Material_Nombre = m.nombre
 WHERE tm.Material_Tipo = 'Relleno' AND tm.Relleno_Densidad IS NOT NULL;
 
 
@@ -160,63 +160,63 @@ WHERE tm.Material_Tipo = 'Relleno' AND tm.Relleno_Densidad IS NOT NULL;
 -- Inserts para tabla Sillon_Modelo
 PRINT 'Insertando datos en tabla Sillon_Modelo...';
 -- ========================================
-SET IDENTITY_INSERT Sillon_Modelo ON;
+SET IDENTITY_INSERT LOS_HELECHOS.Sillon_Modelo ON;
 
-INSERT INTO Sillon_Modelo (codigo_modelo, nombre, descripcion, precio_base)
+INSERT INTO LOS_HELECHOS.Sillon_Modelo (codigo_modelo, nombre, descripcion, precio_base)
 SELECT DISTINCT 
     Sillon_Modelo_Codigo,
     Sillon_Modelo,
     Sillon_Modelo_Descripcion,
     Sillon_Modelo_Precio
 FROM gd_esquema.Maestra
-WHERE Sillon_Modelo_Codigo is not null;
+WHERE LOS_HELECHOS.Sillon_Modelo_Codigo is not null;
 
-SET IDENTITY_INSERT Sillon_Modelo OFF;
+SET IDENTITY_INSERT LOS_HELECHOS.Sillon_Modelo OFF;
 -- ========================================
 -- Inserts para tabla Sillon_Medida
 PRINT 'Insertando datos en tabla Sillon_Medida...';
 -- ========================================
-SET IDENTITY_INSERT Sillon_Modelo ON;
+SET IDENTITY_INSERT LOS_HELECHOS.Sillon_Medida ON;
 
-INSERT INTO Sillon_Medida (alto, ancho, profundidad, precio_medida)
+INSERT INTO LOS_HELECHOS.Sillon_Medida (alto, ancho, profundidad, precio_medida)
 SELECT DISTINCT
     Sillon_Medida_Alto,
     Sillon_Medida_Ancho,
     Sillon_Medida_Profundidad,
     Sillon_Medida_Precio
 FROM gd_esquema.Maestra
-WHERE Sillon_Medida_Alto IS NOT NULL;
+WHERE LOS_HELECHOS.Sillon_Medida_Alto IS NOT NULL;
 
-SET IDENTITY_INSERT Sillon_Modelo OFF;
+SET IDENTITY_INSERT LOS_HELECHOS.Sillon_Medida OFF;
 
 -- ========================================
 -- Inserts para tabla Sillon
 PRINT 'Insertando datos en tabla Sillon...';
 -- ========================================
-INSERT INTO Sillon (codigo_sillon, codigo_modelo, id_medida, id_material)
+INSERT INTO LOS_HELECHOS.Sillon (codigo_sillon, codigo_modelo, id_medida, id_material)
 SELECT 
 	ma.Sillon_Codigo,
     sm.codigo_modelo,
     med.id_medida,
     m.id_material
 FROM gd_esquema.Maestra ma
-JOIN Sillon_Modelo sm 
+JOIN LOS_HELECHOS.Sillon_Modelo sm 
   ON ma.Sillon_Modelo = sm.nombre 
  AND ma.Sillon_Modelo_Descripcion = sm.descripcion
-JOIN Sillon_Medida med 
+JOIN LOS_HELECHOS.Sillon_Medida med 
   ON ma.Sillon_Medida_Alto = med.alto 
  AND ma.Sillon_Medida_Ancho = med.ancho 
  AND ma.Sillon_Medida_Profundidad = med.profundidad
-JOIN Material m ON ma.Material_Nombre = m.nombre;
+JOIN LOS_HELECHOS.Material m ON ma.Material_Nombre = m.nombre;
 
 
 -- ========================================
 -- Inserts para tabla Pedido
 PRINT 'Insertando datos en tabla Pedido...';
 -- ========================================
-SET IDENTITY_INSERT Pedido ON;
+SET IDENTITY_INSERT LOS_HELECHOS.Pedido ON;
 
-INSERT INTO Pedido (nro_pedido, fecha, estado, total, nro_cliente, nro_sucursal)
+INSERT INTO LOS_HELECHOS.Pedido (nro_pedido, fecha, estado, total, nro_cliente, nro_sucursal)
 SELECT DISTINCT 
     Pedido_Numero,
     Pedido_Fecha,
@@ -225,17 +225,17 @@ SELECT DISTINCT
     c.nro_cliente,
     s.nro_sucursal
 FROM gd_esquema.Maestra tm
-JOIN Cliente c ON tm.Cliente_Dni = c.dni
-JOIN Sucursal s ON tm.Sucursal_NroSucursal = s.nro_sucursal
+JOIN LOS_HELECHOS.Cliente c ON tm.Cliente_Dni = c.dni
+JOIN LOS_HELECHOS.Sucursal s ON tm.Sucursal_NroSucursal = s.nro_sucursal
 WHERE Pedido_Numero is not null;
 
-SET IDENTITY_INSERT Pedido OFF;
+SET IDENTITY_INSERT LOS_HELECHOS.Pedido OFF;
 
 -- ========================================
 -- Inserts para tabla Detalle_Pedido
 PRINT 'Insertando datos en tabla Detalle_Pedido...';
 -- ========================================
-INSERT INTO Detalle_Pedido (nro_pedido, id_sillon, cantidad, precio, subtotal)
+INSERT INTO LOS_HELECHOS.Detalle_Pedido (nro_pedido, id_sillon, cantidad, precio, subtotal)
 SELECT DISTINCT 
     Pedido_Numero,
     s.id_sillon,
@@ -243,15 +243,15 @@ SELECT DISTINCT
     Detalle_Pedido_Precio,
     Detalle_Pedido_SubTotal
 FROM gd_esquema.Maestra tm
-JOIN Sillon s ON tm.Sillon_Codigo = s.codigo_sillon;
+JOIN LOS_HELECHOS.Sillon s ON tm.Sillon_Codigo = s.codigo_sillon;
 
 -- ========================================
 -- Inserts para tabla CancelacionPedido
 PRINT 'Insertando datos en tabla CancelacionPedido...';
 -- ========================================
-SET IDENTITY_INSERT CancelacionPedido ON;
+SET IDENTITY_INSERT LOS_HELECHOS.CancelacionPedido ON;
 
-INSERT INTO CancelacionPedido (nro_pedido, fecha_cancelacion, motivo_cancelacion)
+INSERT INTO LOS_HELECHOS.CancelacionPedido (nro_pedido, fecha_cancelacion, motivo_cancelacion)
 SELECT DISTINCT 
     Pedido_Numero,
     Pedido_Cancelacion_Fecha,
@@ -260,14 +260,14 @@ FROM gd_esquema.Maestra
 WHERE Pedido_Cancelacion_Fecha IS NOT NULL;
 
 
-SET IDENTITY_INSERT CancelacionPedido OFF;
+SET IDENTITY_INSERT LOS_HELECHOS.CancelacionPedido OFF;
 -- ========================================
 -- Inserts para tabla Factura
 PRINT 'Insertando datos en tabla Factura...';
 -- ========================================
-SET IDENTITY_INSERT Factura ON;
+SET IDENTITY_INSERT LOS_HELECHOS.Factura ON;
 
-INSERT INTO Factura (nro_factura, nro_sucursal, nro_cliente, fecha, total)
+INSERT INTO LOS_HELECHOS.Factura (nro_factura, nro_sucursal, nro_cliente, fecha, total)
 SELECT DISTINCT 
     Factura_Numero,
     s.nro_sucursal,
@@ -275,11 +275,11 @@ SELECT DISTINCT
     Factura_Fecha,
     Factura_Total
 FROM gd_esquema.Maestra tm
-JOIN Cliente c ON tm.Cliente_Dni = c.dni
-JOIN Sucursal s ON tm.Sucursal_NroSucursal = s.nro_sucursal
+JOIN LOS_HELECHOS.Cliente c ON tm.Cliente_Dni = c.dni
+JOIN LOS_HELECHOS.Sucursal s ON tm.Sucursal_NroSucursal = s.nro_sucursal
 WHERE Factura_Numero is not null;
 
-SET IDENTITY_INSERT Factura OFF;
+SET IDENTITY_INSERT LOS_HELECHOS.Factura OFF;
 
 /**
 
@@ -340,7 +340,7 @@ JOIN Material m ON tm.Material_Nombre = m.nombre;
 **/
 
 
--- Normalización Provincia
+-- Normalizaciï¿½n Provincia
 
 UPDATE Provincia SET nombre_prov = 'Santiago del Estero' WHERE nombre_prov = 'Santia; Del Estero';
 UPDATE Provincia SET nombre_prov = 'Tierra del Fuego' WHERE nombre_prov = 'Tierra Del Fue;';
